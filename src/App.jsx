73bc015360b021499e60c37d64886e6b5d4c2527
jihyunglee <jhlee7125@gmail.com>
2023-06-24 19:19:32 +0900

    feat: import Column on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import Column from 'components/Column/Column';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Column state='Not Started' />
+      <Column state='In Progress' />
+      <Column state='Done' />
+    </div>
+  );
 }
 
 export default App;
