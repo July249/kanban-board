e8c14a1d5fce16d54d8476c682dc004ff980ece0
jihyunglee <jhlee7125@gmail.com>
2023-06-25 01:04:09 +0900

    design: styling Column and importing Task components

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,5 +1,45 @@
+import Task from 'components/Task/Task';
 import style from './Column.module.css';
 
+const tasks = [
+  {
+    id: '1',
+    title: 'Task Title',
+    desc: 'Hello! My name is asdfasdfasdfasdfasdfasdfasdfasdfasdfsadfsadasdfasdfasdfasdfasdfasdasf',
+  },
+  {
+    id: '2',
+    title: 'Task Title',
+    desc: 'Hello!',
+  },
+  {
+    id: '3',
+    title: 'Task Title',
+    desc: 'Hello!',
+  },
+];
+
 export default function Column({ state }) {
-  return <div className={style.column}>{state}</div>;
+  return (
+    <div className={style.column}>
+      <h1
+        className={`${style.column_title} ${
+          state === 'Not Started'
+            ? style.first
+            : state === 'In Progress'
+            ? style.second
+            : style.third
+        }`}
+      >
+        {state}
+      </h1>
+      <ul className={style.tasks}>
+        {tasks.map((task) => (
+          <li className={style.task_item}>
+            <Task task={task} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
