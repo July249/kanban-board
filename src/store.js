a49b901afc78fee9a137c4ff9505ac3bd7132f87
jihyunglee <jhlee7125@gmail.com>
2023-06-25 19:25:52 +0900

    feat: adding template tasks

--- src/store.js
+++ src/store.js
@@ -4,9 +4,21 @@ const store = (set) => ({
   tasks: [
     {
       id: 1,
-      title: 'Task Title',
-      description: 'Task Description',
+      title: 'Task Not Started',
+      description: 'Task Description1',
       status: 'NOT_STARTED',
+    },
+    {
+      id: 2,
+      title: 'Task Progress',
+      description: 'Task Description2',
+      status: 'IN_PROGRESS',
+    },
+    {
+      id: 3,
+      title: 'Task Done',
+      description: 'Task Description3',
+      status: 'DONE',
     },
   ],
 });
