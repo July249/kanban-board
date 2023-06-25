33cf1e995a4e5bceb494902d296a1e91652bf690
jihyunglee <jhlee7125@gmail.com>
2023-06-25 20:12:03 +0900

    design: change App layout since adding TaskInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Column from 'components/Column/Column';
+import TaskInput from 'components/TaskInput/TaskInput';
 import './App.css';
 
 const states = ['NOT_STARTED', 'IN_PROGRESS', 'DONE'];
@@ -6,9 +7,12 @@ const states = ['NOT_STARTED', 'IN_PROGRESS', 'DONE'];
 function App() {
   return (
     <div className='App'>
-      {states.map((state) => (
-        <Column key={state} state={state} />
-      ))}
+      <section>
+        {states.map((state) => (
+          <Column key={state} state={state} />
+        ))}
+      </section>
+      <TaskInput />
     </div>
   );
 }
