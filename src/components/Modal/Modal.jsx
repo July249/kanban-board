4cd82a3db1a6c09bc6de13de5bca7fb10930d24c
jihyunglee <jhlee7125@gmail.com>
2023-06-26 13:17:58 +0900

    feat: adding delete button handler and import deleteTask logic

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,25 +2,40 @@ import { forwardRef } from 'react';
 import classNames from 'classnames';
 import style from './Modal.module.css';
 import { useStore } from '../../store';
+import { shallow } from 'zustand/shallow';
+import threeDigitTag from 'utils/threeDigitTag';
 
-export const Modal = forwardRef(({ modalSwitch }, ref) => {
-  const task = useStore((store) => store.task);
-  console.log(task);
+export const Modal = forwardRef(({ taskId, modalSwitch, onModalToggle }, ref) => {
+  const tasks = useStore((store) => store.tasks.filter((task) => task.id === taskId), shallow);
+  const handleDeleteTask = () => {
+    deleteTask(taskId);
+    onModalToggle(modalSwitch);
+  };
+  const handleModalToggle = () => {
+    onModalToggle(modalSwitch);
+  };
+
+  const task = tasks[0];
+  const tag = !!task ? threeDigitTag(task.tag) : '';
+  const description = !!task ? task.description : '';
+
+  const deleteTask = useStore((store) => store.deleteTask);
+
   return (
-    <div ref={ref} className={classNames(style.modal, `${modalSwitch ? style.active : ''}`)}>
-      <div className={style.modal_wrapper}>
+    <div className={classNames(style.modal, `${modalSwitch && !!task ? style.active : ''}`)}>
+      <div ref={ref} className={style.modal_wrapper}>
         <section className={style.content}>
           <div className={style.task}>
             <h2>
-              Task Tag
-              <span className={style.delete_btn}>
+              {tag}
+              <span className={style.delete_btn} onClick={handleDeleteTask}>
                 <i className='xi-trash'></i>
               </span>
             </h2>
-            <p>Task Description</p>
+            <p>{description}</p>
           </div>
         </section>
-        <div className={style.close_modal}>
+        <div className={style.close_modal} onClick={handleModalToggle}>
           <i className='xi-close'></i>
         </div>
       </div>
