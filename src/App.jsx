a5328f54991c540b08be3f1d47165ecbb35b68cb
jihyunglee <jhlee7125@gmail.com>
2023-06-26 13:16:01 +0900

    feat: import Modal component and adding logic for modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import Column from 'components/Column/Column';
 import TaskInput from 'components/TaskInput/TaskInput';
+import { Modal } from 'components/Modal/Modal';
 import './App.css';
 
 const states = ['NOT_STARTED', 'IN_PROGRESS', 'DONE'];
 
 function App() {
+  const modalRef = useRef(null);
   const [status, setStatus] = useState('NOT_STARTED');
   const [isActive, setIsActive] = useState(false);
+  const [modalSwitch, setModalSwitch] = useState(false);
+  const [taskId, setTaskId] = useState('');
   const onOpenInput = () => setIsActive(true);
   const onCloseInput = () => setIsActive(false);
+  const onModalToggle = (prev) => setModalSwitch(!prev);
   const onCurrentStatus = (status) => {
     setStatus(status);
   };
-
-  console.log(isActive);
+  const onModalTaskId = (taskId) => {
+    setTaskId(taskId);
+  };
 
   return (
     <div className='App'>
@@ -25,10 +31,18 @@ function App() {
             state={state}
             onCurrentStatus={onCurrentStatus}
             onOpenInput={onOpenInput}
+            onModalToggle={onModalToggle}
+            onModalTaskId={onModalTaskId}
           />
         ))}
       </section>
       <TaskInput status={status} isActive={isActive} onCloseInput={onCloseInput} />
+      <Modal
+        ref={modalRef}
+        taskId={taskId}
+        modalSwitch={modalSwitch}
+        onModalToggle={onModalToggle}
+      />
     </div>
   );
 }
