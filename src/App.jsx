147b7eef2fde7d505ad99fe114090905f0ead847
jihyunglee <jhlee7125@gmail.com>
2023-06-26 09:42:07 +0900

    feat: adding open and close input handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Column from 'components/Column/Column';
 import TaskInput from 'components/TaskInput/TaskInput';
 import './App.css';
@@ -5,14 +6,29 @@ import './App.css';
 const states = ['NOT_STARTED', 'IN_PROGRESS', 'DONE'];
 
 function App() {
+  const [status, setStatus] = useState('NOT_STARTED');
+  const [isActive, setIsActive] = useState(false);
+  const onOpenInput = () => setIsActive(true);
+  const onCloseInput = () => setIsActive(false);
+  const onCurrentStatus = (status) => {
+    setStatus(status);
+  };
+
+  console.log(isActive);
+
   return (
     <div className='App'>
       <section>
         {states.map((state) => (
-          <Column key={state} state={state} />
+          <Column
+            key={state}
+            state={state}
+            onCurrentStatus={onCurrentStatus}
+            onOpenInput={onOpenInput}
+          />
         ))}
       </section>
-      <TaskInput />
+      <TaskInput status={status} isActive={isActive} onCloseInput={onCloseInput} />
     </div>
   );
 }
