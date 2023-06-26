d377df32246c864c554ab26be253819fcdbe779d
jihyunglee <jhlee7125@gmail.com>
2023-06-26 11:16:38 +0900

    feat: connect modal toggle function

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,10 +1,13 @@
 import threeDigitTag from 'utils/threeDigitTag';
 import style from './Task.module.css';
 
-export default function Task({ task }) {
+export default function Task({ task, onModalToggle }) {
   const tag = threeDigitTag(task.tag);
+  const handleModalToggle = () => {
+    onModalToggle();
+  };
   return (
-    <section className={style.task}>
+    <section className={style.task} onClick={handleModalToggle}>
       <h2 className={style.tag}>{tag}</h2>
       <p className={style.desc}>{task.description}</p>
     </section>
